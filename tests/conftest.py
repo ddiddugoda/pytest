8547f0bc985ad2a00d98cc2c612ab8bbd05879b2
ddiddugoda <ddiddugoda@wiley.com>
2023-01-08 16:34:33 +0530

    create pytest environment and run test cases

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+"""
+This module contains shared fixtures.
+"""
+
+import pytest
+import selenium.webdriver
+
+@pytest.fixture
+def browser():
+
+    #Initialize the ChromeDriver instance
+    b = selenium.webdriver.Chrome()
+
+    #Make its calls wait up to 10 seconds for elements to appear
+    b.implicitly_wait(10)
+
+    #Return the WebDriver intance for the setup
+    yield b
+
+    #Quit the WebDriver instance for the cleanup
+    b.quit()
+
+   
