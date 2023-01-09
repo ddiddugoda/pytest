932e8172cf9b857823bfaffb96540d5d3c9f9a89
ddiddugoda <ddiddugoda@wiley.com>
2023-01-09 08:06:21 +0530

    Added math function with exception handling

--- pytests/test_maths.py
+++ pytests/test_maths.py
@@ -6,3 +6,29 @@ Their purpose is to show how to use the pytest framework by example.
 # Basic test function
 def test_one_plus_one():
     assert 1 + 1 == 2
+
+
+# Test function to show assertion introspection
+def test_one_plus_two():
+    a = 1
+    b = 2
+    c = 3
+    assert a + b == c
+
+# Test function that verifies an exception
+def test_devide_by_zero():
+    num = 1 / 0
+
+# This contains basic unit tests for math operation
+import pytest
+
+# Test function
+def test_one_plus_one():
+    assert 1 + 1 == 2
+
+# Test function verifies an exception
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+    
+    assert 'division by zero' in str(e.value)
